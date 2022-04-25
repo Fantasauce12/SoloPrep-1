8b0d04802486b022b06762208939ab3b8be2b31d
Jax McKinney <mck20018@byui.edu>
2022-04-25 09:56:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace solo_prep_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name? ");
+            string first = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string last = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last.ToUpper()}, {first} {last}");
+        }
+    }
+}
